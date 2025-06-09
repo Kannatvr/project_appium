0604c4073750c92f9094da20b16d012152466660
Kannatvr <kannadasanengg1@gmail.com>
2025-06-09 12:02:56 +0530

    spec change new

--- test/specs/web/sample.js
+++ test/specs/web/sample.js
@@ -1,6 +1,14 @@
-describe('Android Chrome Web Automation', () => {
-    it('should open Google and search', async () => {
-        await browser.url('https://www.google.com');
-        await browser.pause(5000);
-    });
-});
+// describe('Android Chrome Web Automation', () => {
+//     it('should open Google and search', async () => {
+//         await browser.url('https://www.google.com');
+//         await browser.pause(5000);
+//     });
+// });
+let str="lanimret window"
+let rev="";
+for(let i=str.length-1;i>=0;i--){
+    rev=rev+str[i]
+}
+console.log("output:",rev)
+
+console.log("hiiiii")
